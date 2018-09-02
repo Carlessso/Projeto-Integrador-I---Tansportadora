7fad6304dec3f7d3044a54f896da34a880172c8e
Ramon <Ramon@DESKTOP-JALI1TG>
2018-09-02 15:47:41 -0300

    Inicializar Tela Login

--- src/transoft/TranSOFT.java
+++ src/transoft/TranSOFT.java
@@ -7,10 +7,9 @@ package transoft;
 
 import daos.AuditarDao;
 import entidades.Usuario;
-import telas.FrmPrincipal;
 import log4j.Log4J;
-import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import telas.FrmLogin;
 
 /**
  *
@@ -23,11 +22,11 @@ public class TranSOFT {
     public static Logger log = Logger.getLogger(Log4J.class.getName());
     
     public static void main(String[] args) {
-         log.removeAllAppenders();
+        log.removeAllAppenders();
         
         AUDITORIA = (AuditarDao.auditar()).getAuditar();
 
-        FrmPrincipal form = new FrmPrincipal();
+        FrmLogin form = new FrmLogin();
         form.setVisible(true);
 
     }
